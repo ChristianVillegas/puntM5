c5b2124953e7a82cc019f2fd24bbf657a2d7fea1
Christian <Christian@192.168.56.1>
2019-05-10 23:40:13 +0200

    Exercicis 1-2-3 dels Tests JUnit

---
+++ puntM5/src/Punt.java
@@ -0,0 +1,35 @@
+
+public class Punt {
+	private int x;
+	private int y;
+
+	public Punt() {
+		this(0, 0);
+	}
+
+	public Punt(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void suma(Punt p) {
+		x += p.x;
+		y += p.y;
+	}
+}
---
+++ puntM5/src/TestPunt.java
@@ -0,0 +1,54 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPunt {
+	@Test
+	public void testPuntConstructor() {
+		// comprova que el constructor Punt() pugui ser cridat
+		Punt p = new Punt();
+	}
+
+	@Test
+	public void testPuntBuit() {
+		// comprova que el constructor Punt() deixi a 0 els valors
+		Punt p = new Punt();
+		Assert.assertEquals(0, p.getX()); // Afirma que la coordenada x val 0
+		Assert.assertEquals(0, p.getY());
+	}
+
+	@Test
+	public void testPuntSetXAssignaCorrectament() {
+		Punt p = new Punt();
+		p.setX(12);
+		Assert.assertEquals(12, p.getX());
+	}
+
+	@Test
+	public void testPuntSetYAssignaCorrectament() {
+		Punt p = new Punt();
+		p.setY(12);
+		Assert.assertEquals(12, p.getY());
+	}
+
+	@Test
+	public void testPuntDefinit() {
+		// comprova que el constructor Punt(int,int) deixi els valors correctes
+		Punt p = new Punt(3, 4);
+		Assert.assertEquals(3, p.getX());
+		Assert.assertEquals(4, p.getY());
+	}
+
+	@Test
+	public void testSuma() {
+		// comprova que el métode suma(Punt p) funcioni correctament
+		Punt p = new Punt(5, 2);
+		Punt q = new Punt(5, 4);
+		p.suma(q);
+		Assert.assertEquals(10, p.getX());
+		Assert.assertEquals(6, p.getY());
+	}
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("TestPunt");
+	}
+}
