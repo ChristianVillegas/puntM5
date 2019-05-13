48ec1620a4269ec4f48d83a2a53cdb8ea89dc991
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-05-13 18:46:57 +0200

    Classe 'Punt' modificat per l'array coord[2]

--- puntM5/src/Punt.java
+++ puntM5/src/Punt.java
@@ -1,35 +1,34 @@
 
 public class Punt {
-	private int x;
-	private int y;
+	int[] coord = new int[2];
 
 	public Punt() {
 		this(0, 0);
 	}
 
 	public Punt(int x, int y) {
-		this.x = x;
-		this.y = y;
+		this.coord[0] = x;
+		this.coord[1] = y;
 	}
 
 	public int getX() {
-		return x;
+		return coord[0];
 	}
 
 	public int getY() {
-		return y;
+		return coord[1];
 	}
 
 	public void setX(int x) {
-		this.x = x;
+		this.coord[0] = x;
 	}
 
 	public void setY(int y) {
-		this.y = y;
+		this.coord[1] = y;
 	}
 
 	public void suma(Punt p) {
-		x += p.x;
-		y += p.y;
+		coord[0] += p.coord[0];
+		coord[1] += p.coord[1];
 	}
 }
