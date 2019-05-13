5b416052381912aa1609c08a0bd6904a372c481e
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-05-13 18:26:47 +0200

    Classe i tests Segment finalitzats

--- puntM5/src/Segment.java
+++ puntM5/src/Segment.java
@@ -1,54 +1,28 @@
 
 public class Segment {
-	private int x1;
-	private int x2;
-	private int y1;
-	private int y2;
+	Punt p1;
+	Punt p2;
 
 	public Segment() {
-		this(0, 0, 0, 0);
+		p1 = new Punt();
+		p2 = new Punt();
 	}
 
-	public Segment(int x1, int x2, int y1, int y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
+	public Segment(Punt p1, Punt p2) {
+		this.p1 = p1;
+		this.p2 = p2;
 	}
 
-	public int getX1() {
-		return x1;
-	}
-
-	public int getY1() {
-		return y1;
-	}
-	
-	public int getX2() {
-		return x2;
+	/* Retorna el punt segons l'índex (1 o 2)*/
+	public Punt get(int index) {
+		switch(index) {
+		case 1: return p1;
+		case 2: return p2;
+		default: throw new IllegalArgumentException("Només tinc els punts 1 i 2");
+		}
 	}
 	
-	public int getY2() {
-		return y2;
-	}
-
-	public void setX1(int x1) {
-		this.x1 = x1;
-	}
-
-	public void setY1(int y1) {
-		this.y1 = y1;
-	}
-	
-	public void setX2(int x2) {
-		this.x2 = x2;
-	}
-	
-	public void setY2(int y2) {
-		this.y2 = y2;
-	}
-	
-	public void longitud(Segment s) {
-	double longitud = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+	public double longitud() {
+		return Math.sqrt(Math.pow(p1.getX()-p2.getX(), 2) + Math.pow(p1.getY()-p2.getY(), 2));
 	}
 }
--- puntM5/src/TestSegment.java
+++ puntM5/src/TestSegment.java
@@ -4,59 +4,54 @@ import org.junit.Assert;
 public class TestSegment {
 	@Test
 	public void testSegmentConstructor() {
-		// comprova que el constructor Punt() pugui ser cridat
-		Segment s = new Segment();
+		// comprova que el constructor Segment() pugui ser cridat
+		Punt p1 = new Punt();
+		Punt p2 = new Punt();
 	}
 
 	@Test
 	public void testSegmentBuit() {
-		// comprova que el constructor Punt() deixi a 0 els valors
-		Segment s = new Segment();
-		Assert.assertEquals(0, s.getX1()); // Afirma que la coordenada x1 val 0
-		Assert.assertEquals(0, s.getX2());
-		Assert.assertEquals(0, s.getY1());
-		Assert.assertEquals(0, s.getY2());
+		// comprova que el constructor Segment() deixi a 0 els valors
+		Punt p1 = new Punt();
+		Punt p2 = new Punt();
+		Segment s = new Segment(p1, p2);
+		Assert.assertEquals(0, s.get(1).getX());
+		Assert.assertEquals(0, s.get(1).getY());
+		Assert.assertEquals(0, s.get(2).getX());
+		Assert.assertEquals(0, s.get(2).getY());
 	}
 
 	@Test
-	public void testSegmentSetX1AssignaCorrectament() {
+	public void testLongitudSegmentPerDefecte() {
+		//comprova que el mï¿½tode longitud() per defecte funcioni correctament
 		Segment s = new Segment();
-		s.setX1(12);
-		Assert.assertEquals(12, s.getX1());
-	}
-	
-	@Test
-	public void testSegmentSetX2AssignaCorrectament() {
-		Segment s = new Segment();
-		s.setX2(10);
-		Assert.assertEquals(10, s.getX2());
+		double longitudEsperada = 0;
+		Assert.assertEquals(longitudEsperada, s.longitud(), 0.000001);
 	}
 
-	@Test
-	public void testSegmentSetY1AssignaCorrectament() {
-		Segment s = new Segment();
-		s.setY1(12);
-		Assert.assertEquals(12, s.getY1());
-	}
-	
-	@Test
-	public void testSegmentSetY2AssignaCorrectament() {
-		Segment s = new Segment();
-		s.setY2(10);
-		Assert.assertEquals(10, s.getY2());
-	}
 	
 	@Test
 	public void testLongitud() {
-		//comprova que el métode longitud(Segment s) funcioni correctament
-		Segment s = new Segment(10, 3, 8, 4);
-		s.longitud(s);
-		Assert.assertEquals(100, s.getX1());
-		Assert.assertEquals(9, s.getX2());
-		Assert.assertEquals(64, s.getY1());
-		Assert.assertEquals(16, s.getY2());
+		//comprova que el mï¿½tode longitud() funcioni correctament
+		Punt p1 = new Punt(3, 5);
+		Punt p2 = new Punt(6, 10);
+		Segment s = new Segment(p1, p2);
+		double longitudEsperada = 5.830951894845301;
+		Assert.assertEquals(longitudEsperada, s.longitud(), 0.000001);
 	}
 
+	@Test
+	public void testLongitud2() {
+		//segona prova canviant els valors del mÃ¨tode longitud()
+		Punt p1 = new Punt(5, 3);
+		Punt p2 = new Punt(7, 1);
+		Segment s = new Segment(p1, p2);
+		double longitudEsperada = 2.8284271247461903;
+		Assert.assertEquals(longitudEsperada, s.longitud(), 0.000001);
+	}
+
+	
+	
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("TestSegment");
 	}
